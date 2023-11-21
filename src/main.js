23d36572489fcc9dd8cc864e7933446ab9d11d25
lijinfeng704@ <863934520@qq.com>
2023-11-21 20:50:32 +0800

    移动端优化300ms

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import Icon from '@/base/mm-icon/mm-icon.vue'
 import VueLazyload from 'vue-lazyload'
 import '@/styles/index.scss'
 import defaultImg from '@/assets/images/default.png'
+import fastclick from 'fastclick';
 const app = createApp(App);
 
 app.component('mmIcon',Icon);
@@ -15,6 +16,9 @@ app.use(VueLazyload, {
     preload: 1.3,
     loading: defaultImg
 })
+
+// 优化移动端300ms点击延迟
+fastclick(document.body);
 
 app.use(router).use(store).use(mmToast).mount('#mmPlayer');
 
