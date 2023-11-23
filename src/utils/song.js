c24dfe32403a0345026000f4804c0f3adcd886e9
lijinfeng704@ <863934520@qq.com>
2023-11-23 17:17:38 +0800

    格式化

--- src/utils/song.js
+++ src/utils/song.js
@@ -1,70 +1,50 @@
-import { toHttps } from './util'
+import { toHttps } from "./util";
 
 function filterSinger(singers) {
-  let arr = []
-  singers.forEach(item => {
-    arr.push(item.name)
-  })
-  return arr.join('/')
+  if (!Array.isArray(singers) || !singers.length) {
+    return "";
+  }
+  let arr = [];
+  singers.forEach((item) => {
+    arr.push(item.name);
+  });
+  return arr.join("/");
 }
 
 export class Song {
   constructor({ id, name, singer, album, image, duration, url }) {
-    this.id = id
-    this.name = name
-    this.singer = singer
-    this.album = album
-    this.image = image
-    this.duration = duration
-    this.url = url
+    this.id = id;
+    this.name = name;
+    this.singer = singer;
+    this.album = album;
+    this.image = image;
+    this.duration = duration;
+    this.url = url;
   }
 }
 
 export function createPlayList(music) {
+  const album = music.album || music.al || {};
+  const duration = music.duration || music.dt;
   return new Song({
     id: music.id,
     name: music.name,
-    singer: music.artists.length > 0 && filterSinger(music.artists),
-    album: music.album.name,
-    image: toHttps(music.album.picUrl) || null,
-    duration: music.duration / 1000,
-    url: `https://music.163.com/song/media/outer/url?id=${music.id}.mp3`
-  })
-}
-
-export function createTopList(music) {
-  return new Song({
-    id: music.id,
-    name: music.name,
-    singer: music.ar.length > 0 && filterSinger(music.ar),
-    album: music.al.name,
-    image: toHttps(music.al.picUrl),
-    duration: music.dt / 1000,
-    url: `https://music.163.com/song/media/outer/url?id=${music.id}.mp3`
-  })
+    singer: filterSinger(music.ar || music.artists),
+    album: album.name,
+    image: toHttps(album.picUrl) || null,
+    duration: duration / 1000,
+    url: `https://music.163.com/song/media/outer/url?id=${music.id}.mp3`,
+  });
 }
 
 // 歌曲数据格式化
-const formatSongs = function formatPlayList(list) {
-  let Songs = []
-  list.forEach(item => {
-    const musicData = item
+export const formatSongs = function formatPlayList(list) {
+  const Songs = [];
+  list.forEach((item) => {
+    const musicData = item;
     if (musicData.id) {
-      Songs.push(createPlayList(musicData))
+      Songs.push(createPlayList(musicData));
     }
-  })
-  return Songs
-}
-
-export const formatTopSongs = function formatTopList(list) {
-  let Songs = []
-  list.forEach(item => {
-    const musicData = item
-    if (musicData.id) {
-      Songs.push(createTopList(musicData))
-    }
-  })
-  return Songs
-}
-
-export default formatSongs
+  });
+  return Songs;
+};
